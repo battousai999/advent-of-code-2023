0142e4986783b7bb984064bde93aa69128ee2c6e
Kurtis Jones <battousai999@gmail.com>
2023-12-03 23:05:29 -0700

    Day 4 problems

---
+++ day-04-part-01/Program.fs
@@ -0,0 +1,44 @@
+﻿open System
+open System.IO
+open System.Text.RegularExpressions
+
+type ScratchCard = {
+    CardNumber : int
+    WinningNumbers : int list
+    ContestNumbers : int list
+}
+
+let (|Regexer|_|) (regex : Regex) input =
+    let m = regex.Match(input)
+
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
+
+let rawScratchCards = File.ReadAllLines("scratchcards.txt")
+
+let cardRegex = Regex(@"^Card\s+(\d+): ([\d\s]+) \| ([\d\s]+)$", RegexOptions.IgnoreCase)
+
+let scratchCards = 
+    rawScratchCards
+        |> Array.map (
+            fun line -> 
+                match line with
+                | Regexer cardRegex [cardNumber; winningNumbersStr; contestNumbersStr] ->
+                    let winningNumbers = winningNumbersStr.Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Array.map Int32.Parse |> Array.toList
+                    let contestNumbers = contestNumbersStr.Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Array.map Int32.Parse |> Array.toList
+
+                    { CardNumber = cardNumber |> Int32.Parse; WinningNumbers = winningNumbers; ContestNumbers = contestNumbers }
+                | _ -> raise <| ApplicationException("Invalid line in input: " + line))
+        |> Array.toList
+
+let scratchCardPoints =
+    scratchCards
+        |> List.map (
+            fun scratchCard -> 
+                let numWinners = scratchCard.ContestNumbers |> List.where (fun x -> scratchCard.WinningNumbers |> List.contains x) |> List.length
+
+                if numWinners = 0 then 0 else pown 2 (numWinners - 1))
+
+let sumOfPoints = scratchCardPoints |> List.sum
+
+printfn "%d" sumOfPoints
---
+++ day-04-part-02/Program.fs
@@ -0,0 +1,49 @@
+﻿open System
+open System.IO
+open System.Text.RegularExpressions
+
+type ScratchCard = {
+    CardNumber : int
+    WinningNumbers : int list
+    ContestNumbers : int list
+}
+
+let (|Regexer|_|) (regex : Regex) input =
+    let m = regex.Match(input)
+
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
+
+let rawScratchCards = File.ReadAllLines("../day-04-part-01/scratchcards.txt")
+
+let cardRegex = Regex(@"^Card\s+(\d+): ([\d\s]+) \| ([\d\s]+)$", RegexOptions.IgnoreCase)
+
+let scratchCards = 
+    rawScratchCards
+        |> Array.map (
+            fun line -> 
+                match line with
+                | Regexer cardRegex [cardNumber; winningNumbersStr; contestNumbersStr] ->
+                    let winningNumbers = winningNumbersStr.Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Array.map Int32.Parse |> Array.toList
+                    let contestNumbers = contestNumbersStr.Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Array.map Int32.Parse |> Array.toList
+
+                    { CardNumber = cardNumber |> Int32.Parse; WinningNumbers = winningNumbers; ContestNumbers = contestNumbers }
+                | _ -> raise <| ApplicationException("Invalid line in input: " + line))
+        |> Array.toList
+
+let initialBuckets = scratchCards |> List.map (fun card -> 1) |> List.toArray
+
+let scratchCardFolder ((index, counters) : int * int array) (card : ScratchCard) =
+    let numWinners = card.ContestNumbers |> List.where (fun x -> card.WinningNumbers |> List.contains x) |> List.length
+
+    [1..numWinners] |> List.iter (fun index -> counters[(card.CardNumber - 1) + index] <- counters[(card.CardNumber - 1) + index] + counters[card.CardNumber - 1])
+
+    (index + 1, counters)
+
+let (_, processedScratchCards) =
+    scratchCards
+        |> List.fold scratchCardFolder (0, initialBuckets)
+
+let sumOfPoints = processedScratchCards |> Array.sum
+
+printfn "%d" sumOfPoints
