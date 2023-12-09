e1d6aa64bac5e9aa23f4f3f827d5915b0e76e04b
Kurtis Jones <battousai999@gmail.com>
2023-12-08 23:23:32 -0700

    Day 9 problems

---
+++ day-09-part-01/Program.fs
@@ -0,0 +1,46 @@
+﻿open System.IO
+open System
+open System.Linq
+
+let rawReport = File.ReadAllLines("report.txt")
+
+let report = 
+    rawReport 
+        |> Array.map (
+            fun line -> 
+                line.Split(' ')
+                    |> Array.map Int32.Parse
+                    |> Array.toList)
+        |> Array.toList
+
+let rec predictValue (history: int list) =
+    let rec last list =
+        match list with
+        | [x] -> x
+        | _::tail -> last tail
+        | _ -> failwith "empty list"
+    let (subHistory, _) = 
+        history
+            |> List.fold (
+                fun ((results, prevValue): ResizeArray<int> * int option) (x: int) ->
+                    match prevValue with
+                    | Some value ->
+                        results.Add(x - value)
+                        (results, Some x)
+                    | None ->
+                        (results, Some x))
+                (new ResizeArray<int>(), None)
+    
+    if subHistory.All(fun x -> x = 0) then
+        (history |> last)
+    else
+        (history |> last) + (predictValue (subHistory |> Seq.toList))
+    
+let sumOfPredictedValues = 
+    report 
+        |> List.map predictValue
+        |> List.sum
+
+printfn "%d" sumOfPredictedValues
+
+
---
+++ day-09-part-02/Program.fs
@@ -0,0 +1,41 @@
+﻿open System.IO
+open System
+open System.Linq
+
+let rawReport = File.ReadAllLines("../day-09-part-01/report.txt")
+
+let report = 
+    rawReport 
+        |> Array.map (
+            fun line -> 
+                line.Split(' ')
+                    |> Array.map Int32.Parse
+                    |> Array.toList)
+        |> Array.toList
+
+let rec predictInitialValue (history: int list) =
+    let (subHistory, _) = 
+        history
+            |> List.fold (
+                fun ((results, prevValue): ResizeArray<int> * int option) (x: int) ->
+                    match prevValue with
+                    | Some value ->
+                        results.Add(x - value)
+                        (results, Some x)
+                    | None ->
+                        (results, Some x))
+                (new ResizeArray<int>(), None)
+    
+    if subHistory.All(fun x -> x = 0) then
+        (history |> List.head)
+    else
+        (history |> List.head) - (predictInitialValue (subHistory |> Seq.toList))
+    
+let sumOfPredictedValues = 
+    report 
+        |> List.map predictInitialValue
+        |> List.sum
+
+printfn "%d" sumOfPredictedValues
+
+
