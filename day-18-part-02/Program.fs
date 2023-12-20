5be2ff8654524cda18611364acc857af1e9690b5
Kurtis Jones <kurtis.jones@extendhealth.com>
2023-12-19 17:03:48 -0700

    Day 18 part 2 implementation

--- day-18-part-02/Program.fs
+++ day-18-part-02/Program.fs
@@ -14,13 +14,13 @@ type MapCellType =
 | InnerTrench
 
 type Point = {
-    X: int
-    Y: int
+    X: int64
+    Y: int64
 }
 
 type Instruction = {
     Direction: Direction
-    Length: int
+    Length: int64
     RgbCode: string
 }
 
@@ -32,26 +32,26 @@ let (|Regexer|_|) (regex : Regex) input =
 
 let instructionRegex = Regex(@"^(\w)\s(\d+)\s\(#([\da-f]{6})\)$")
 
-// let rawDigPlan = File.ReadAllLines("dig-plan.txt")
+let rawDigPlan = File.ReadAllLines("../day-18-part-01/dig-plan.txt")
 
-let rawDigPlanStr = @"R 6 (#70c710)
-D 5 (#0dc571)
-L 2 (#5713f0)
-D 2 (#d2c081)
-R 2 (#59c680)
-D 2 (#411b91)
-L 5 (#8ceee2)
-U 2 (#caa173)
-L 1 (#1b58a2)
-U 2 (#caa171)
-R 2 (#7807d2)
-U 3 (#a77fa3)
-L 2 (#015232)
-U 2 (#7a21e3)"
+// let rawDigPlanStr = @"R 6 (#70c710)
+// D 5 (#0dc571)
+// L 2 (#5713f0)
+// D 2 (#d2c081)
+// R 2 (#59c680)
+// D 2 (#411b91)
+// L 5 (#8ceee2)
+// U 2 (#caa173)
+// L 1 (#1b58a2)
+// U 2 (#caa171)
+// R 2 (#7807d2)
+// U 3 (#a77fa3)
+// L 2 (#015232)
+// U 2 (#7a21e3)"
 
-let rawDigPlan = rawDigPlanStr.Split(Environment.NewLine)
+// let rawDigPlan = rawDigPlanStr.Split(Environment.NewLine)
 
-let digPlan =
+let initialDigPlan =
     rawDigPlan
         |> Array.map (
             fun line ->
@@ -64,9 +64,26 @@ let digPlan =
                         | "L" -> Direction.Left
                         | "R" -> Direction.Right
                         | _ -> raise <| ApplicationException($"unexpected direction: {directionStr}")
-                    { Direction = direction; Length = length |> Int32.Parse; RgbCode = rgbCode }
+                    { Direction = direction; Length = length |> Int64.Parse; RgbCode = rgbCode }
                 | _ -> raise <| ApplicationException($"line did not match instruction format: {line}"))
         |> Array.toList
+
+let digPlan =
+    initialDigPlan
+        |> List.map (
+            fun instruction ->
+                let rawLength = instruction.RgbCode[0..4]
+                let rawDirection = instruction.RgbCode.[5]
+                let length = Convert.ToInt32(rawLength, 16)
+                let direction =
+                    match rawDirection with
+                    | '0' -> Direction.Right
+                    | '1' -> Direction.Down
+                    | '2' -> Direction.Left
+                    | '3' -> Direction.Up
+                    | _ -> raise <| ApplicationException($"unexpected direction: {rawDirection}")
+
+                { Direction = direction; Length = length; RgbCode = instruction.RgbCode })
 
 let startingPoint = { X = 0; Y = 0 }
 
@@ -77,10 +94,10 @@ let points =
                 fun ((prevPoint, list): Point * ResizeArray<Point>) (instruction: Instruction) ->
                     let nextPoint =
                         match instruction.Direction with
-                        | Direction.Up -> { X = prevPoint.X; Y = prevPoint.Y - (instruction.Length+1) }
-                        | Direction.Down -> { X = prevPoint.X; Y = prevPoint.Y + (instruction.Length+1) }
-                        | Direction.Left -> { X = prevPoint.X - (instruction.Length+1); Y = prevPoint.Y }
-                        | Direction.Right -> { X = prevPoint.X + (instruction.Length+1); Y = prevPoint.Y }
+                        | Direction.Up -> { X = prevPoint.X; Y = prevPoint.Y - instruction.Length }
+                        | Direction.Down -> { X = prevPoint.X; Y = prevPoint.Y + instruction.Length }
+                        | Direction.Left -> { X = prevPoint.X - instruction.Length; Y = prevPoint.Y }
+                        | Direction.Right -> { X = prevPoint.X + instruction.Length; Y = prevPoint.Y }
                     list.Add(nextPoint)
                     (nextPoint, list))
                 (startingPoint, ResizeArray<Point>())
@@ -94,19 +111,24 @@ let points =
 
     // (lastPoint :: startingPoint :: (tailPoints |> Seq.toList)) |> Array.ofList
 
-let points2 = [|
-    { X = 0; Y = 0};
-    { X = 0; Y = 10};
-    { X = 10; Y = 10};
-    { X = 10; Y = 0};
-|]
+// let points2 = [|
+//     { X = 0; Y = 0};
+//     { X = 0; Y = 10};
+//     { X = 10; Y = 10};
+//     { X = 10; Y = 0};
+// |]
 
-printfn "%A" points
+// printfn "%A" points
 
 let area =
     let size = Array.length points
     //let sum = [1..(Array.length points)-1] |> List.sumBy (fun i -> points[i].X * (points[(i+1) % size].Y - points[i-1].Y))
     let sum = [0..(Array.length points)-1] |> List.sumBy (fun i -> (points[i].X * points.[(i+1) % size].Y) - (points.[(i+1) % size].X * points.[i].Y))
-    abs(sum / 2)
+    let perimeter = digPlan |> List.sumBy (fun instruction -> instruction.Length)
+
+    printfn "%d" sum
+    printfn "%d" perimeter
+
+    abs(sum / 2L) + (perimeter / 2L) + 1L
 
 printfn "%d" area
